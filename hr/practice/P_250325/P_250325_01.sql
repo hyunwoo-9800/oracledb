aa8f7aa7c9c601fd678ab797b7d030f14b188031
hyunwoo-9800 <hyunoo_9800@naver.com>
2025-03-26 14:22:22 +0900

    [update] 오라클 연습 정답 추가 2025/03/26 @1.0h

--- hr/practice/P_250325/P_250325_01.sql
+++ hr/practice/P_250325/P_250325_01.sql
@@ -6,6 +6,12 @@ FROM
     EMPLOYEES A
      LEFT JOIN JOBS B
         ON A.JOB_ID = B.JOB_ID
+		
+select  first_name,job_id,salary, hire_date 
+from employees 
+where to_char(hire_date,'yyyy') = 2000 
+and job_id in ( select job_id from jobs where max_salary > 10000)		
+		
 WHERE
     A.HIRE_DATE > '1999/12/31'
     AND B.MAX_SALARY > 10000;
@@ -27,6 +33,11 @@ WHERE
         WHERE
             FIRST_NAME = 'Michael'
     );
+	
+select * from departments where manager_id in 
+(select employee_id 
+from employees where  upper(first_name) like '%MICHAEL%');
+
 
 -- 직종의 최저임금이 사번 105번 사원의 임금보다 적은 직종를 조회
 SELECT
@@ -46,6 +57,10 @@ WHERE
         WHERE
             EMPLOYEE_ID = 105
     );
+	
+select * from jobs where min_salary < 
+(select salary from employees where employee_id = 105);
+
 
 -- 메일주소에 밑줄표시가 있는 사원을 조회
 SELECT
@@ -55,6 +70,10 @@ FROM
 WHERE
     INSTR(EMAIL, '_') > 0;
 
+--select * from employees where email like '%\_%' ESCAPE '\';
+
+
+
 -- 사원 이름과 해당 사원의 매니저 이름을 조회
 SELECT
     A.*,
@@ -63,6 +82,10 @@ FROM
     EMPLOYEES A
      LEFT JOIN EMPLOYEES B
         ON A.MANAGER_ID = B.EMPLOYEE_ID;
+		
+select e1.first_name Employee, e2.first_name Manager 
+from employees e1, employees e2 where  e1.manager_id  = e2.employee_id;
+
 
 -- 30번 부서에 입사한 사원의 수를 연도별로 조회
 SELECT
@@ -98,6 +121,10 @@ GROUP BY
     A.DEPARTMENT_ID
 ORDER BY
     A.HIRE_DATE;
+	
+select to_char(hire_date,'yyyy') , count(*) from employees 
+where department_id = 30 group by to_char(hire_date,'yyyy');
+
 
 -- 각 부서의 직종별로 사원들의 총 급여합을 구해보자
 SELECT
@@ -117,6 +144,11 @@ ORDER BY
     B.DEPARTMENT_NAME,
     C.JOB_ID;
 
+select department_id department, job_id job, sum(salary) TotalSalary 
+from employees group by department_id, job_id 
+order by department_id, job_id;
+
+
 -- 업무별 최저임금과 최고임금 사이의 급여을 가지는 사원의 이름과 업무명을 조회
 SELECT
     A.FIRST_NAME || ' ' || A.LAST_NAME AS "이름",
@@ -129,12 +161,21 @@ WHERE
         B.MIN_SALARY >= A.SALARY
     AND A.SALARY <= B.MAX_SALARY;
 
+select first_name, job_title from employees e, jobs j 
+where salary between min_salary and max_salary order by first_name;
+
+
 -- 커미션을 받은 사원과 받지 않은 사원이 각각 몇명인지 조회
 SELECT
     COUNT(CASE WHEN COMMISSION_PCT IS NOT NULL THEN 'a' END) AS "커미션을 받는 사원",
     COUNT(CASE WHEN COMMISSION_PCT IS NULL THEN 'b' END) AS "커미션을 받지 않는 사원"
 FROM
     EMPLOYEES;
+
+select count(commission_pct) NoEmployeesWithCommission, 
+count(*) - count(commission_pct) NoEmployeesWithoutCommssion 
+from employees
+
 
 -- 2월 28일에 입사한 사원의 이름, 업무명, 부서명을 조회
 -- 해당일에 입사한 사원이 없어서 2월달로 설정
@@ -153,6 +194,12 @@ FROM
         ON A.DEPARTMENT_ID = D.DEPARTMENT_ID
 WHERE
     TO_CHAR(A.HIRE_DATE) LIKE '_____02%';
+	
+select first_name, job_title, department_name 
+from employees e, jobs j, departments d 
+where e.job_id = j.job_id and e.department_id = d.department_id 
+and to_char(hire_date,'ddmm') = '2802'	
+	
 
 -- 최저임금이 10000보다 높은 업무의 세부사항을 조회
 SELECT
@@ -163,6 +210,11 @@ FROM
         ON A.JOB_ID = B.JOB_ID
 WHERE
     A.MIN_SALARY > 10000;
+	
+	
+SELECT * FROM JOBS WHERE MIN_SALARY > 10000
+
+	
 
 --  2002년~2005년 사이에 입사한 사원의 이름과 입사일을 조회
 SELECT
@@ -173,6 +225,10 @@ WHERE
     TO_CHAR(HIRE_DATE) BETWEEN '2002' AND '2005'
 ORDER BY
     HIRE_DATE;
+
+SELECT FIRST_NAME, HIRE_DATE FROM EMPLOYEES 
+WHERE TO_CHAR(HIRE_DATE, 'YYYY') BETWEEN 2002 AND 2005 ORDER BY HIRE_DATE
+
 
 --  IT Programmer이거나 Sales Man인 사원의 이름과 입사일을 조회
 SELECT
@@ -185,6 +241,10 @@ FROM
 WHERE
     C.JOB_ID LIKE 'IT%PROG'
     OR C.JOB_ID LIKE 'SA%MAN';
+	
+SELECT FIRST_NAME, HIRE_DATE 
+FROM EMPLOYEES WHERE JOB_ID IN ('IT_PROG', 'SA_MAN');
+	
 
 -- 2008년 1월 1일 이후에 입사한 사원을 조회
 SELECT
@@ -193,6 +253,9 @@ FROM
     EMPLOYEES
 WHERE
     HIRE_DATE > '2008/01/01';
+	
+SELECT * FROM EMPLOYEES where hire_date > '01-jan-2008';	
+	
 
 -- 사번이 150,160번인 사원을 조회
 SELECT
@@ -202,6 +265,9 @@ FROM
 WHERE
     EMPLOYEE_ID = 150
     OR EMPLOYEE_ID = 160;
+
+SELECT * FROM EMPLOYEES WHERE EMPLOYEE_ID in (150,160);
+
 
 -- 임금이 10000보다 적은 사원의 이름, 임금, 커미션, 입사일을 조회
 SELECT
@@ -214,6 +280,10 @@ FROM
 WHERE
     SALARY < 10000;
 
+SELECT FIRST_NAME, SALARY, COMMISSION_PCT, HIRE_DATE 
+FROM EMPLOYEES WHERE SALARY < 10000;
+
+
 -- 최고 임금이 10000~20000인 업무의 업무명, 최저임금과 최고임금의 차액을 조회
 SELECT
     JOB_TITLE,
@@ -223,12 +293,20 @@ FROM
 WHERE
     MAX_SALARY BETWEEN 10000 AND 20000;
 
+SELECT JOB_TITLE, MAX_SALARY-MIN_SALARY DIFFERENCE FROM JOBS WHERE MAX_SALARY 
+BETWEEN 10000 AND 20000;
+
+
+
 -- 사원 이름과 임금을 출력하되 임금을 1000단위로 조회
 SELECT
     FIRST_NAME || ' ' || LAST_NAME AS "이름",
     TO_CHAR(SALARY, 'FM999,999,999')
 FROM
     EMPLOYEES;
+
+SELECT FIRST_NAME, SALARY, ROUND(SALARY, -3) FROM EMPLOYEES;
+
 
 -- 업무명을 기준으로 하여 내림차순으로 업무 세부사항을 조회
 SELECT
@@ -240,6 +318,9 @@ FROM
 ORDER BY
     B.JOB_TITLE DESC;
 
+SELECT * FROM JOBS ORDER BY JOB_TITLE desc;
+
+
 --  성이나 이름이 S로 시작하는 사원을 조회
 SELECT
     *
@@ -249,6 +330,11 @@ WHERE
     FIRST_NAME LIKE 'S%'
     OR LAST_NAME LIKE 'S%';
 
+
+SELECT FIRST_NAME, LAST_NAME FROM EMPLOYEES WHERE FIRST_NAME LIKE 'S%' OR 
+LAST_NAME LIKE 'S%';
+
+
 -- 5월에 입사한 사원을 조회
 SELECT
     *
@@ -256,6 +342,9 @@ FROM
     EMPLOYEES
 WHERE
     TO_CHAR(HIRE_DATE) LIKE '_____05%';
+
+SELECT * FROM EMPLOYEES WHERE TO_CHAR(HIRE_DATE, 'MON')= 'MAY';
+
 
 -- 커미션을 받지 않고, 임금이 5000~10000이면서 30번 부서에 소속된 사원을 조회
 SELECT
@@ -269,12 +358,19 @@ WHERE
       AND A.SALARY BETWEEN 5000 AND 10000 )
     AND B.DEPARTMENT_ID = 30;
 
+SELECT * FROM EMPLOYEES WHERE COMMISSION_PCT IS NULL AND SALARY BETWEEN 5000 
+AND 10000 AND DEPARTMENT_ID=30;
+
+
 -- 사원들의 이름과 첫 월급날을 조회 매월 1일이 월급 날이다
 SELECT
     FIRST_NAME || ' ' || LAST_NAME AS "이름",
     TRUNC(HIRE_DATE, 'MM') AS "첫 월급날"
 FROM
     EMPLOYEES;
+
+SELECT FIRST_NAME, HIRE_DATE, LAST_DAY(HIRE_DATE)+1 FROM EMPLOYEES;
+
 
 -- 사원의 이름과 근속연수를 조회
 SELECT
@@ -285,6 +381,9 @@ SELECT
 FROM
     EMPLOYEES;
 
+SELECT FIRST_NAME, HIRE_DATE, FLOOR((SYSDATE-HIRE_DATE)/365)FROM EMPLOYEES;
+
+
 -- 2001년에 입사한 사원의 이름을 조회
 SELECT
     FIRST_NAME || ' ' || LAST_NAME AS "이름"
@@ -293,6 +392,9 @@ FROM
 WHERE
     TO_CHAR(HIRE_DATE) LIKE '2001%';
 
+SELECT FIRST_NAME, HIRE_DATE FROM EMPLOYEES WHERE TO_CHAR(HIRE_DATE, 'YYYY')=2001;
+
+
 -- 첫 글자는 대문자로, 나머지 글자는 소문자로 변환하여 성과 이름을 조회, initcap 사용
 SELECT
     INITCAP(FIRST_NAME) AS "성",
@@ -300,11 +402,18 @@ SELECT
 FROM
     EMPLOYEES;
 
+SELECT INITCAP(FIRST_NAME), INITCAP(LAST_NAME) FROM EMPLOYEES;
+
+
 -- 업무명의 첫 단어를 조회
 SELECT
     SUBSTR(JOB_TITLE, 1, 1) AS "업무명 첫 단어"
 FROM
     JOBS;
+
+
+SELECT JOB_TITLE, SUBSTR( JOB_TITLE, 1, INSTR(JOB_TITLE || ' ', ' ')-1 ) FROM JOBS; 
+
 
 -- 성의 3번째 문자 이후에 'b'가 포함된 사원의 이름의 길이를 조회
 SELECT
@@ -313,6 +422,9 @@ FROM
     EMPLOYEES
 WHERE
     FIRST_NAME LIKE '___b%';
+
+SELECT FIRST_NAME, LAST_NAME FROM EMPLOYEES WHERE INSTR(LAST_NAME,'B') > 3
+
 
 -- 이름과 이메일주소가 대소문자 상관없이 같은 사원의 이름을 대문자로, 이메일주소는 소문자로 조회
 SELECT
@@ -323,6 +435,9 @@ FROM
 WHERE
     INITCAP(LAST_NAME) = INITCAP(EMAIL);
 
+SELECT UPPER(FIRST_NAME), LOWER(EMAIL) FROM EMPLOYEES WHERE UPPER(FIRST_NAME)=UPPER(EMAIL); 
+
+
 -- 올해 입사한 사원을 조회
 SELECT
     *
@@ -331,11 +446,18 @@ FROM
 WHERE
     TO_CHAR(HIRE_DATE, 'YYYY') = TO_CHAR(SYSDATE, 'YYYY');
 
+
+SELECT * FROM EMPLOYEES WHERE TO_CHAR(HIRE_DATE,'YYYY')=TO_CHAR(SYSDATE, 'YYYY'); 
+
+
 -- 현재 날짜와 2011년 1월 1일이 며칠 차이인지 조회
 SELECT
     TO_DATE(TO_CHAR(SYSDATE, 'YYYY-mm-DD')) - TO_DATE('2011-01-01', 'YYYY-mm-DD')
 FROM
     DUAL;
+
+SELECT SYSDATE - to_date('01-jan-2011') FROM DUAL;
+
 
 -- 올해 각 달마다 몇 명의 사원이 입사하였는지 조회
 SELECT
@@ -346,6 +468,11 @@ FROM
 --where to_char(HIRE_DATE, 'YY') = to_char(sysdate, 'YY')
 GROUP BY
     TO_CHAR(HIRE_DATE, 'YY/mm');
+
+SELECT TO_CHAR(HIRE_DATE,'MM'), COUNT (*) FROM EMPLOYEES 
+WHERE TO_CHAR(HIRE_DATE,'YYYY')= TO_CHAR(SYSDATE,'YYYY') 
+GROUP BY TO_CHAR(HIRE_DATE,'MM');
+
 
 -- 매니저 사번과 그 매니저가 총 몇명을 관리하는지 조회
 SELECT
@@ -358,6 +485,9 @@ FROM
 GROUP BY
     A.MANAGER_ID;
 
+SELECT MANAGER_ID, COUNT(*) FROM EMPLOYEES GROUP BY MANAGER_ID
+
+
 -- 사원 사번과 이전 업무를 종료한 날을 조회
 SELECT
     A.EMPLOYEE_ID,
@@ -367,6 +497,10 @@ FROM
      LEFT JOIN JOB_HISTORY B
         ON A.JOB_ID = B.JOB_ID;
 
+
+SELECT EMPLOYEE_ID, MAX(END_DATE) FROM JOB_HISTORY GROUP BY EMPLOYEE_ID;
+
+
 -- 입사일이 n월 '15일' 이후인(몇월이든 월말에 입사한) 사원의 수를 조회
 SELECT
     COUNT(*) AS "월말 입사 사원 수"
@@ -374,6 +508,9 @@ FROM
     EMPLOYEES
 WHERE
     TO_CHAR(HIRE_DATE, 'dd') > 15;
+
+SELECT COUNT(*) FROM EMPLOYEES WHERE TO_CHAR(HIRE_DATE,'DD') > 15
+
 
 -- 국가번호와 해당 국가에 있는 도시들의 개수를 조회
 SELECT
@@ -385,6 +522,9 @@ FROM
         ON A.COUNTRY_ID = B.COUNTRY_ID
 GROUP BY
     A.COUNTRY_ID;
+
+SELECT COUNTRY_ID, COUNT(*) FROM LOCATIONS GROUP BY COUNTRY_ID;
+
 
 -- 각 부서별로 커미션을 받은 사원들의 평균임금을 조회
 SELECT
@@ -398,6 +538,9 @@ WHERE
     A.COMMISSION_PCT IS NOT NULL
 GROUP BY
     A.DEPARTMENT_ID;
+
+SELECT DEPARTMENT_ID, AVG(SALARY) FROM EMPLOYEES 
+WHERE COMMISSION_PCT IS NOT NULL GROUP BY DEPARTMENT_ID;
 
 
 -- 업무별로 업무명, 사원의 수, 임금의 합, 최고임금과 최저임금의 차액을 조회
@@ -416,6 +559,10 @@ GROUP BY
     A.DEPARTMENT_ID,
     ABS(C.MAX_SALARY - C.MIN_SALARY);
 
+
+SELECT JOB_ID, COUNT(*), SUM(SALARY), MAX(SALARY)-MIN(SALARY) SALARY FROM 
+EMPLOYEES GROUP BY JOB_ID;
+
 -- 평균임금이 10000을 넘는 업무의 업무ID와 평균임금을 조회
 SELECT
     JOB_ID,
@@ -427,6 +574,11 @@ GROUP BY
 HAVING
     AVG(SALARY) > 10000;
 
+
+
+SELECT JOB_ID, AVG(SALARY) FROM EMPLOYEES 
+GROUP BY JOB_ID HAVING AVG(SALARY)>10000
+
 -- 10명이 넘는 사원이 입사한 연도를 조회
 SELECT
     TO_CHAR(HIRE_DATE, 'YYYY') AS "입사년도"
@@ -436,6 +588,10 @@ GROUP BY
     TO_CHAR(HIRE_DATE, 'YYYY')
 HAVING
     COUNT(TO_CHAR(HIRE_DATE, 'YYYY')) > 10;
+
+
+SELECT TO_CHAR(HIRE_DATE,'YYYY') FROM EMPLOYEES 
+GROUP BY TO_CHAR(HIRE_DATE,'YYYY') HAVING COUNT(EMPLOYEE_ID) > 10
 
 -- 커미션을 받은 사원이 5명 이상인 부서를 조회
 SELECT
@@ -451,6 +607,11 @@ GROUP BY
 HAVING
     COUNT(A.COMMISSION_PCT) >= 5;
 
+
+SELECT DEPARTMENT_ID FROM EMPLOYEES WHERE COMMISSION_PCT IS NOT NULL 
+GROUP BY DEPARTMENT_ID HAVING COUNT(COMMISSION_PCT)>5;
+
+
 -- 과거에 하나가 넘는(하나 이상 말고 초과)의 업무를 하였던 사원의 사번을 조회
 SELECT
     A.EMPLOYEE_ID
@@ -465,6 +626,9 @@ GROUP BY
 HAVING
     COUNT(A.EMPLOYEE_ID) > 1;
 
+SELECT EMPLOYEE_ID FROM JOB_HISTORY GROUP BY EMPLOYEE_ID HAVING COUNT(*) > 1;
+
+
 -- 3명이 넘는 사원이 100일이 넘게 근무한 업무ID를 조회
 SELECT
     B.JOB_ID
@@ -478,6 +642,10 @@ GROUP BY
     B.JOB_ID
 HAVING
     COUNT(B.JOB_ID) > 3;
+
+SELECT JOB_ID FROM JOB_HISTORY WHERE END_DATE-START_DATE > 100 
+GROUP BY JOB_ID HAVING COUNT(*)>3;
+
 
 -- 부서 번호, 연도, 연도별 입사한 사원의 수를 조인하여 조회
 SELECT
@@ -495,6 +663,10 @@ ORDER BY
     TO_CHAR(A.HIRE_DATE, 'YYYY'),
     B.DEPARTMENT_ID;
 
+SELECT DEPARTMENT_ID, TO_CHAR(HIRE_DATE,'YYYY'), COUNT(EMPLOYEE_ID) FROM EMPLOYEES 
+GROUP BY DEPARTMENT_ID, TO_CHAR(HIRE_DATE, 'YYYY') ORDER BY DEPARTMENT_ID;
+
+
 -- 임의의 매니저가 5명 이상의 사원을 관리하고 있는 부서를 조회
 SELECT
     C.DEPARTMENT_NAME
@@ -506,6 +678,11 @@ GROUP BY
     C.DEPARTMENT_NAME
 HAVING
     COUNT(A.MANAGER_ID) > 5;
+	
+	
+SELECT DISTINCT DEPARTMENT_ID FROM EMPLOYEES 
+GROUP BY DEPARTMENT_ID, MANAGER_ID HAVING COUNT(EMPLOYEE_ID) > 5;	
+	
 
 -- 사번 115번 사원의 현재 임금이 6000미만일 경우 8000으로 변경하시오
 SELECT
@@ -530,6 +707,9 @@ WHERE
     )
     AND EMPLOYEE_ID = 115;
 
+
+UPDATE EMPLOYEES SET SALARY = 8000 WHERE EMPLOYEE_ID = 115 AND SALARY < 6000;
+
 ROLLBACK;
 
 -- 20번 부서를 삭제
@@ -545,6 +725,8 @@ WHERE
 
 ROLLBACK;
 
+DELETE FROM DEPARTMENTS WHERE DEPARTMENT_ID=20;
+
 -- 사번 110번 사원이 10번 부서에 속해있고
 -- 현재 업무ID가 IT로 시작하지 않으면 해당 사원의 업무ID를 'IT_PROG'로 변경
 SELECT
@@ -556,6 +738,9 @@ FROM
 WHERE
         A.EMPLOYEE_ID = 110
     AND B.DEPARTMENT_ID = 10;
+
+UPDATE EMPLOYEES SET JOB_ID= 'IT_PROG' 
+WHERE EMPLOYEE_ID=110 AND DEPARTMENT_ID=10 AND NOT JOB_ID LIKE 'IT%'
 
 -- 부서 테이블에 매니저ID가 120이고 장소ID는 도쿄 시의 임의의 장소로 하는 행을 추가
 -- 부서 번호가 없어서 00으로 insert test
@@ -578,6 +763,8 @@ INSERT INTO DEPARTMENTS VALUES ( 00,
 
 ROLLBACK;
 
+INSERT INTO DEPARTMENTS (150,'SPORTS',120,1200);
+
 -- 부서 이름과 해당 부서의 사원들 수를 조회
 SELECT
     B.DEPARTMENT_NAME        AS "부서명",
@@ -588,6 +775,10 @@ FROM
         ON A.DEPARTMENT_ID = B.DEPARTMENT_ID
 GROUP BY
     B.DEPARTMENT_NAME;
+	
+SELECT DEPARTMENT_NAME, COUNT(*) FROM EMPLOYEES JOIN DEPARTMENTS USING 
+(DEPARTMENT_ID) GROUP BY DEPARTMENT_NAME;	
+	
 
 -- 업무기록에서 30번 부서의 모든 업무에 대하여 업무명, 사번, 시작일과 종료일 간 근무일수를 조회
 SELECT
@@ -599,6 +790,11 @@ FROM
      LEFT JOIN DEPARTMENTS B
         ON A.DEPARTMENT_ID = B.DEPARTMENT_ID;
 
+
+SELECT EMPLOYEE_ID, JOB_TITLE, END_DATE-START_DATE DAYS 
+FROM JOB_HISTORY NATURAL JOIN JOBS WHERE DEPARTMENT_ID=30;
+
+
 -- 부서명과 매니저 이름을 조회
 SELECT
     B.DEPARTMENT_NAME,
@@ -608,13 +804,8 @@ FROM
      LEFT JOIN DEPARTMENTS B
         ON A.MANAGER_ID = B.MANAGER_ID;
 
-SELECT
-    A.DEPARTMENT_NAME,
-    B.FIRST_NAME || ' ' || B.LAST_NAME AS "이름"
-FROM
-    DEPARTMENTS A
-     LEFT JOIN EMPLOYEES B
-        ON A.MANAGER_ID = B.MANAGER_ID;
+SELECT DEPARTMENT_NAME, FIRST_NAME FROM DEPARTMENTS D JOIN EMPLOYEES E ON 
+(D.MANAGER_ID=E.EMPLOYEE_ID);
 
 -- 부서명과 매니저 이름, 도시를 조회
 SELECT
@@ -628,16 +819,8 @@ FROM
      LEFT JOIN LOCATIONS C
         ON B.LOCATION_ID = C.LOCATION_ID;
 
-SELECT
-    A.DEPARTMENT_NAME,
-    B.FIRST_NAME || ' ' || B.LAST_NAME AS "이름",
-    C.CITY
-FROM
-    DEPARTMENTS A
-     LEFT JOIN EMPLOYEES B
-        ON A.MANAGER_ID = B.MANAGER_ID
-     LEFT JOIN LOCATIONS C
-        ON A.LOCATION_ID = C.LOCATION_ID;
+SELECT DEPARTMENT_NAME, FIRST_NAME, CITY FROM DEPARTMENTS D JOIN EMPLOYEES E ON 
+(D.MANAGER_ID=E.EMPLOYEE_ID) JOIN LOCATIONS L USING (LOCATION_ID)
 
 -- 국가명, 도시, 부서명을 조회
 SELECT
@@ -650,6 +833,11 @@ FROM
         ON A.COUNTRY_ID = B.COUNTRY_ID
      LEFT JOIN DEPARTMENTS C
         ON B.LOCATION_ID = C.LOCATION_ID;
+
+SELECT COUNTRY_NAME, CITY, DEPARTMENT_NAME 
+FROM COUNTRIES JOIN LOCATIONS USING (COUNTRY_ID) 
+JOIN DEPARTMENTS USING (LOCATION_ID);
+
 
 -- 2000~2005년 사이의 모든 업무에 대하여 업무명, 부서명, 사원의 성, 업무시작일을 조회
 SELECT
@@ -668,6 +856,12 @@ FROM
 WHERE
     TO_CHAR(A.END_DATE, 'YYYY') BETWEEN '2000' AND '2005';
 
+
+SELECT JOB_TITLE, DEPARTMENT_NAME, LAST_NAME, START_DATE 
+FROM JOB_HISTORY JOIN JOBS USING (JOB_ID) JOIN DEPARTMENTS 
+USING (DEPARTMENT_ID) JOIN EMPLOYEES USING (EMPLOYEE_ID) 
+WHERE TO_CHAR(START_DATE,'YYYY') BETWEEN 2000 AND 2005;
+
 -- 현재 임금이 15000 이상인 사원이 지금까지 수행했던 업무들을 조회
 SELECT
     B.JOB_ID
@@ -677,6 +871,11 @@ FROM
         ON A.EMPLOYEE_ID = B.EMPLOYEE_ID
 WHERE
     A.SALARY >= 15000;
+
+
+SELECT JH.* 
+FROM JOB_HISTORY JH JOIN EMPLOYEES E ON (JH.EMPLOYEE_ID = E.EMPLOYEE_ID) 
+WHERE SALARY > 15000;
 
 -- 5년 이상 근무한 모든 매니저들의 부서명, 매니저 이름, 매니저의 임금을 조회
 SELECT DISTINCT
@@ -694,6 +893,11 @@ WHERE
 ORDER BY
     C.DEPARTMENT_NAME;
 
+
+SELECT DEPARTMENT_NAME, FIRST_NAME, SALARY 
+FROM DEPARTMENTS D JOIN EMPLOYEES E ON (D.MANAGER_ID=E.MANAGER_ID) 
+WHERE (SYSDATE-HIRE_DATE) / 365 > 5;
+
 -- 자신의 매니저보다 먼저 입사한 사원 이름을 조회
 SELECT
     A.FIRST_NAME || ' ' || A.LAST_NAME AS "이름"
@@ -706,6 +910,11 @@ WHERE
 ORDER BY
     A.HIRE_DATE,
     B.HIRE_DATE;
+
+SELECT FIRST_NAME FROM EMPLOYEES E1 JOIN EMPLOYEES E2 ON 
+(E1.MANAGER_ID=E2.EMPLOYEE_ID) 
+WHERE E1.HIRE_DATE < E2.HIRE_DATE;
+
 
 -- 사원이 6개월 미만으로 근무한 업무에 대하여 사원 이름, 업무명을 조회
 SELECT
@@ -720,6 +929,12 @@ FROM
 WHERE
     TO_CHAR(TRUNC(MOD(MONTHS_BETWEEN(A.END_DATE, A.START_DATE) / 12, 1) * 12)) < 6;
 
+
+SELECT FIRST_NAME, JOB_TITLE FROM EMPLOYEES E JOIN JOB_HISTORY JH ON 
+(JH.EMPLOYEE_ID = E.EMPLOYEE_ID) 
+JOIN JOBS J ON( JH.JOB_ID = J.JOB_ID) WHERE 
+MONTHS_BETWEEN(END_DATE,START_DATE) < 6;
+
 -- 최고임금이 10000이 넘는 부서의 세부사항을 조회 
 SELECT
     A.*
@@ -732,6 +947,11 @@ FROM
 WHERE
     C.MAX_SALARY > 10000;
 
+SELECT * FROM DEPARTMENTS WHERE DEPARTMENT_ID IN 
+( SELECT DEPARTMENT_ID FROM EMPLOYEES 
+GROUP BY DEPARTMENT_ID HAVING MAX(SALARY)>10000);
+
+
 -- 'Smith'에 의해 관리되는 부서의 세부사항을 조회
 SELECT
     *
@@ -742,6 +962,10 @@ FROM
 WHERE
     LAST_NAME = 'Smith';
 
+SELECT * FROM DEPARTMENTS WHERE MANAGER_ID IN 
+(SELECT EMPLOYEE_ID FROM EMPLOYEES WHERE FIRST_NAME='SMITH');
+
+
 -- 과거에 다른 어떠한 업무도 수행하지 않은 사원을 조회
 SELECT
     DISTINCT A.*
@@ -751,6 +975,10 @@ FROM
         ON A.DEPARTMENT_ID = B.DEPARTMENT_ID
 WHERE
     A.EMPLOYEE_ID <> B.EMPLOYEE_ID;
+
+SELECT * FROM EMPLOYEES WHERE EMPLOYEE_ID NOT IN 
+(SELECT EMPLOYEE_ID FROM JOB_HISTORY);
+
 
 -- 과거에 다른 업무를 수행했던 사원들 중에 최고임금이 10000 이상인 사원이 속한 부서의 세부사항을 조회
 SELECT
@@ -766,7 +994,13 @@ FROM
 WHERE
     A.EMPLOYEE_ID = B.EMPLOYEE_ID
     AND C.MAX_SALARY >= 10000;
-    
+ 
+SELECT * FROM DEPARTMENTS 
+WHERE DEPARTMENT_ID IN 
+(SELECT DEPARTMENT_ID FROM EMPLOYEES 
+WHERE EMPLOYEE_ID IN (SELECT EMPLOYEE_ID FROM JOB_HISTORY) 
+GROUP BY DEPARTMENT_ID HAVING MAX(SALARY) >10000);
+ 
     
 -- 과거에 IT Programmer로 일했던 사원들의 현재 업무를 조회
 SELECT
@@ -777,3 +1011,8 @@ FROM
     LEFT JOIN JOBS        C ON A.JOB_ID = C.JOB_ID
 WHERE
     B.JOB_ID = 'IT_PROG';
+	
+
+SELECT * FROM JOBS WHERE JOB_ID IN 
+(SELECT JOB_ID FROM EMPLOYEES WHERE EMPLOYEE_ID IN 
+(SELECT EMPLOYEE_ID FROM JOB_HISTORY WHERE JOB_ID='IT_PROG'));	
